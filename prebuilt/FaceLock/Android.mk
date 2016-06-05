5cd8e33289107a92e1eb9fc7a015410d9e40c142
Lok Jian Ming <jianminglok@hotmail.com>
2016-06-05 12:39:31 +0800

    Update Android.mk

--- prebuilt/FaceLock/Android.mk
+++ prebuilt/FaceLock/Android.mk
@@ -2,7 +2,7 @@
 LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE        := EngineerMode
+LOCAL_MODULE        := FaceLock
 #LOCAL_MODULE_OWNER  := qcom
 LOCAL_MODULE_TAGS   := optional
 LOCAL_MODULE_CLASS  := APPS

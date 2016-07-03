1be4fd50317bcb20e57d32bb15d5d5c2919207eb
Lok Jian Ming <jianminglok@hotmail.com>
2016-07-03 12:01:07 +0800

    Update apply.sh

--- patches/apply.sh
+++ patches/apply.sh
@@ -3,7 +3,6 @@ cd frameworks/av
 git apply -v ../../device/xiaomi/mt6795-common/patches/frameworks_av/0001-libmedia-stub-out-MTK-specific-bits-audio-working.patch
 git apply -v ../../device/xiaomi/mt6795-common/patches/frameworks_av/0002-camera.patch
 git apply -v ../../device/xiaomi/mt6795-common/patches/frameworks_av/0003-frameworks_av_fmradio_patch.patch
-git apply -v ../../device/xiaomi/mt6795-common/patches/frameworks_av/0001_engineering_mode_audio_settings_frameworks_av.patch
 cd ../..
 cd frameworks/base
 git apply -v ../../device/xiaomi/mt6795-common/patches/frameworks_base/0001-location-support-GNSS-extension-presumably-from-MTK.patch
@@ -12,7 +11,6 @@ git apply -v ../../device/xiaomi/mt6795-common/patches/frameworks_base/0003-loca
 git apply -v ../../device/xiaomi/mt6795-common/patches/frameworks_base/0006-add-com.mediatek-for-build.patch
 git apply -v ../../device/xiaomi/mt6795-common/patches/frameworks_base/0007-Update-for-mtk.patch
 git apply -v ../../device/xiaomi/mt6795-common/patches/frameworks_base/0004_minimal_xlog_frameworks_base.patch 
-git apply -v ../../device/xiaomi/mt6795-common/patches/frameworks_base/0001_engineering_mode_audio_settings_frameworks_base.patch
 git apply -v ../../device/xiaomi/mt6795-common/patches/frameworks_base/0005-enable-virtual-keys.patch
 cd ../..
 cd frameworks/opt/telephony
@@ -29,7 +27,6 @@ git apply -v ../../device/xiaomi/mt6795-common/patches/system_netd/0001-hotpost-
 cd ../..
 cd hardware/libhardware
 git apply -v ../../device/xiaomi/mt6795-common/patches/hardware_libhardware/0001-fix-gps-light-audio.patch
-git apply -v ../../device/xiaomi/mt6795-common/patches/hardware_libhardware/0001_engineering_mode_audio_settings_hardware_libhardware.patch
 cd ../..
 cd hardware/libhardware_legacy
 git apply -v ../../device/xiaomi/mt6795-common/patches/hardware_libhardware_legacy/0001-patch-for-mtk.patch
